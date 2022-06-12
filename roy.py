213c4688cd46847388d98ab809752259c590bcab
Sameer <107326224+Sameer1648@users.noreply.github.com>
2022-06-13 00:41:07 +0900

    Create roy.py
    https://github.com/Sameer1648/MB.git
    
    cd MB
    
    python roy.py

---
+++ roy.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# !python
+# cython: language_level=3
+""" 
+-------------------------------------------------------------------------
+- Cod BY : SidraELEzz
+- Github : https://github.com/SidraELEzz
+- Telegram: https://t.me/SidraTools
+- Telegram: https://t.me/tt_rq
+-------------------------------------------------------------------------
+     
+""" 
+import os,sys
+try:
+	import requests
+except ImportError:
+	os.system("pip install requests")
+
+try:
+	import user_agent
+except ImportError:
+	os.system("pip install user_agent")
+
+import os
+import sys
+import time
+import json
+import random
+import threading
+import re
+import requests
+from time import sleep
+from user_agent import generate_user_agent
+from random import choice
+
+A = "\033[1;91m"
+B = "\033[1;90m"
+C = "\033[1;97m"
+E = "\033[1;92m"
+H = "\033[1;93m"
+K = "\033[1;94m"
+L = "\033[1;95m"
+
+def xa():
+	banera ="""
+  \033[1;92m888     888  .d8888b.  8888888888 8888888b.  
+  \033[1;97m888     888 d88P  Y88b 888        888   Y88b 
+  \033[1;92m888     888 Y88b.      888        888    888 
+  \033[1;97m888     888  "Y888b.   8888888    888   d88P 
+  \033[1;92m888     888     "Y88b. 888        8888888P"  
+  \033[1;97m888     888       "888 888        888 T88b   
+  \033[1;92mY88b. .d88P Y88b  d88P 888        888  T88b  
+   \033[1;97m"Y88888P"   "Y8888P"  8888888888 888   T88b 
+                                                                                
+  \033[1;93m < \033[1;92mTHE TOOL IS FREE\033[1;93m >  \033[1;91m 
+ ---------------------------
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mAUTHOR     : SIDRA ELEZZ
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mTelegram   : SIDRATOOLS
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mYOUTUBE    : TERMUX TOOLS
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mGITHUB     : GITHUB.COM/SIDRAELEZZ\033[1;91m
+  ---------------------------
+"""     
+
+	for sidra in banera.splitlines():
+		time.sleep(0.05)
+		print(sidra)
+		
+		
+def xs():
+	banera ="""
+  \033[1;92m888     888  .d8888b.  8888888888 8888888b.  
+ 
